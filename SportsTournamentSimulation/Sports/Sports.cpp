3dca3bc06f802930d4df74e43c4552a981e86444
Pavithra8805 <pavithra8805@gmail.com>
2024-09-24 13:24:41 +0000

    Added Encapsulation

--- SportsTournamentSimulation/Sports/Sports.cpp
+++ SportsTournamentSimulation/Sports/Sports.cpp
@@ -5,71 +5,91 @@
 using namespace std;
 
 // Player class definition
-class Player {
+class Player
+{
 private:
-    string playerName;
+    string playerName; // Private member variable
 
 public:
     // Constructor to initialize player name
     Player(string name) : playerName(name) {}
 
-    // Method to get the player's name
-    string getPlayerName() const {
+    // Method to get the player's name (Accessor)
+    string getPlayerName() const
+    {
         return playerName;
+    }
+
+    // Method to set the player's name (Mutator)
+    void setPlayersName(string name)
+    {
+        playerName = name;
     }
 };
 
 // Team class definition
-class Team {
+class Team
+{
 private:
-    string teamName;
-    vector<Player> players;
+    string teamName;        // Private member variable
+    vector<Player> players; // Private member variable
 
     // Static variable to count the number of teams
     static int teamCount;
 
 public:
     // Constructor to initialize team name and players
-    Team(string name, vector<Player> teamPlayers) : teamName(name), players(teamPlayers) {
+    Team(string name, vector<Player> teamPlayers) : teamName(name), players(teamPlayers)
+    {
         teamCount++; // Increment team count when a new team is created
     }
 
     // Method to display team details
-    void displayTeam() const {
+    void displayTeam() const
+    {
         cout << "Team: " << teamName << "\nPlayers: ";
-        for (const Player& player : players) {
+        for (const Player &player : players)
+        {
             cout << player.getPlayerName() << " ";
         }
         cout << endl;
     }
 
     // Method to add a player to the team
-    void addPlayer(Player newPlayer) {
-        if (players.size() < 11) {  // Assuming a maximum of 11 players per team
+    void addPlayer(Player newPlayer)
+    {
+        if (players.size() < 11)
+        { // Assuming a maximum of 11 players per team
             players.push_back(newPlayer);
             cout << newPlayer.getPlayerName() << " added to " << teamName << endl;
-        } else {
+        }
+        else
+        {
             cout << "Cannot add more players to " << teamName << ". Team is full." << endl;
         }
     }
 
     // Method to get the team name
-    string getTeamName() const {
+    string getTeamName() const
+    {
         return teamName;
     }
 
     // Method to get the number of players in the team
-    int getPlayerCount() const {
+    int getPlayerCount() const
+    {
         return players.size();
     }
 
     // Static method to get the total number of teams created
-    static int getTeamCount() {
+    static int getTeamCount()
+    {
         return teamCount;
     }
 
     // Static method to reset the team count
-    static void resetTeamCount() {
+    static void resetTeamCount()
+    {
         teamCount = 0;
     }
 };
@@ -78,51 +98,63 @@ public:
 int Team::teamCount = 0;
 
 // Match class definition
-class Match {
+class Match
+{
 private:
-    Team* team1;
-    Team* team2;
-    string result;
+    Team *team1;   // Private member variable
+    Team *team2;   // Private member variable
+    string result; // Private member variable
 
     // Static variable to count the number of matches played
     static int matchCount;
 
 public:
     // Constructor to initialize the match with two teams
-    Match(Team* t1, Team* t2) : team1(t1), team2(t2) {
+    Match(Team *t1, Team *t2) : team1(t1), team2(t2)
+    {
         result = "Match not played yet.";
         matchCount++; // Increment match count when a new match is created
     }
 
     // Method to simulate the match and determine the winner
-    void playMatch() {
-        if (team1->getPlayerCount() > team2->getPlayerCount()) {
+    void playMatch()
+    {
+        if (team1->getPlayerCount() > team2->getPlayerCount())
+        {
             result = team1->getTeamName() + " wins!";
-        } else if (team1->getPlayerCount() < team2->getPlayerCount()) {
+        }
+        else if (team1->getPlayerCount() < team2->getPlayerCount())
+        {
             result = team2->getTeamName() + " wins!";
-        } else {
+        }
+        else
+        {
             result = "It's a draw!";
         }
         cout << "Result: " << result << endl;
     }
 
     // Method to display the result of the match
-    void displayResult() const {
+    void displayResult() const
+    {
         cout << "Match Result: " << result << endl;
     }
 
     // Static method to get the total number of matches played
-    static int getMatchCount() {
+    static int getMatchCount()
+    {
         return matchCount;
     }
 
     // Static method to reset the match count
-    static void resetMatchCount() {
+    static void resetMatchCount()
+    {
         matchCount = 0;
     }
 
     // Destructor to clean up dynamically allocated memory
-    ~Match() {
+    ~Match()
+    {
         delete team1;
         delete team2;
     }
@@ -132,9 +164,11 @@ public:
 int Match::matchCount = 0;
 
 // Function to get player names from the user
-vector<Player> getPlayers(int numPlayers) {
+vector<Player> getPlayers(int numPlayers)
+{
     vector<Player> players;
-    for (int i = 0; i < numPlayers; ++i) {
+    for (int i = 0; i < numPlayers; ++i)
+    {
         string playerName;
         cout << "Enter name of player " << (i + 1) << ": ";
         cin >> playerName;
@@ -144,7 +178,8 @@ vector<Player> getPlayers(int numPlayers) {
 }
 
 // Main function
-int main() {
+int main()
+{
     // Get team A details from the user
     string teamAName;
     int numPlayersA;
@@ -153,7 +188,7 @@ int main() {
     cout << "Enter the number of players in Team A: ";
     cin >> numPlayersA;
     vector<Player> playersA = getPlayers(numPlayersA);
-    Team* teamA = new Team(teamAName, playersA);
+    Team *teamA = new Team(teamAName, playersA);
 
     // Get team B details from the user
     string teamBName;
@@ -163,7 +198,7 @@ int main() {
     cout << "Enter the number of players in Team B: ";
     cin >> numPlayersB;
     vector<Player> playersB = getPlayers(numPlayersB);
-    Team* teamB = new Team(teamBName, playersB);
+    Team *teamB = new Team(teamBName, playersB);
 
     // Display team details
     cout << "\nInitial Teams:\n";
@@ -184,7 +219,7 @@ int main() {
 
     // Create and play a match between the two teams
     cout << "\nPlaying a match between " << teamA->getTeamName() << " and " << teamB->getTeamName() << "...\n";
-    Match* match1 = new Match(teamA, teamB);
+    Match *match1 = new Match(teamA, teamB);
     match1->playMatch();
     match1->displayResult();
 
