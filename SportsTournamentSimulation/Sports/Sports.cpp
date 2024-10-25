54e27085d41c8b3d511395b3975a58d8834e9a0c
Pavithra8805 <pavithra8805@gmail.com>
2024-10-25 10:51:20 +0530

    Inheritance

--- SportsTournamentSimulation/Sports/Sports.cpp
+++ SportsTournamentSimulation/Sports/Sports.cpp
@@ -4,181 +4,140 @@
 
 using namespace std;
 
-// Player class definition
-class Player
-{
+// Base class for common team properties
+class SportsTeam {
 private:
-    string playerName; // Private member variable
+    string teamName;
 
 public:
-    // Default constructor
-    Player()
-    {
-        playerName = "Unknown"; // Default player name
-        cout << "Player Default Constructor called.\n";
-    }
+    SportsTeam(string name) : teamName(name) {}
 
-    // Parameterized constructor to initialize player name
-    Player(string name) : playerName(name)
-    {
-        cout << "Player Parameterized Constructor called for " << playerName << ".\n";
+    string getTeamName() const {
+        return teamName;
     }
+};
 
-    // Destructor
-    ~Player()
-    {
-        cout << "Player Destructor called for " << playerName << ".\n";
-    }
+// Player class definition
+class Player {
+private:
+    string playerName;
 
-    // Method to get the player's name (Accessor)
-    string getPlayerName() const
-    {
+public:
+    Player(string name) : playerName(name) {}
+
+    string getPlayerName() const {
         return playerName;
     }
 
-    // Method to set the player's name (Mutator)
-    void setPlayersName(string name)
-    {
+    void setPlayersName(string name) {
         playerName = name;
     }
 };
 
-// Team class definition
-class Team
-{
+// Team class definition inheriting from SportsTeam
+class Team : public SportsTeam {
 private:
-    string teamName;        // Private member variable
-    vector<Player> players; // Private member variable
-
-    static int teamCount; // Static variable to count the number of teams
+    vector<Player> players;
+    static int teamCount;
 
 public:
-    // Default constructor
-    Team()
-    {
-        teamName = "Unknown Team";
+    Team(string name, vector<Player> teamPlayers) : SportsTeam(name), players(teamPlayers) {
         teamCount++;
-        cout << "Team Default Constructor called.\n";
     }
 
-    // Parameterized constructor to initialize team name and players
-    Team(string name, vector<Player> teamPlayers) : teamName(name), players(teamPlayers)
-    {
-        teamCount++;
-        cout << "Team Parameterized Constructor called for " << teamName << ".\n";
-    }
-
-    // Destructor
-    ~Team()
-    {
-        cout << "Team Destructor called for " << teamName << ".\n";
-        teamCount--; // Decrease team count when a team is destroyed
-    }
-
-    // Method to display team details
-    void displayTeam() const
-    {
-        cout << "Team: " << teamName << "\nPlayers: ";
-        for (const Player &player : players)
-        {
+    void displayTeam() const {
+        cout << "Team: " << getTeamName() << "\nPlayers: ";
+        for (const Player& player : players) {
             cout << player.getPlayerName() << " ";
         }
         cout << endl;
     }
 
-    // Method to add a player to the team
-    void addPlayer(Player newPlayer)
-    {
-        if (players.size() < 11)
-        { // Assuming a maximum of 11 players per team
+    void addPlayer(Player newPlayer) {
+        if (players.size() < 11) {
             players.push_back(newPlayer);
-            cout << newPlayer.getPlayerName() << " added to " << teamName << endl;
-        }
-        else
-        {
-            cout << "Cannot add more players to " << teamName << ". Team is full." << endl;
+            cout << newPlayer.getPlayerName() << " added to " << getTeamName() << endl;
+        } else {
+            cout << "Cannot add more players to " << getTeamName() << ". Team is full." << endl;
         }
     }
 
-    // Static method to get the total number of teams created
-    static int getTeamCount()
-    {
+    int getPlayerCount() const {
+        return players.size();
+    }
+
+    static int getTeamCount() {
         return teamCount;
     }
+
+    static void resetTeamCount() {
+        teamCount = 0;
+    }
 };
 
-int Team::teamCount = 0; // Initialize static team count
+// Initialize static variable for Team
+int Team::teamCount = 0;
 
-// Match class definition
-class Match
-{
+// Match class definition inheriting from SportsTeam
+class Match : public SportsTeam {
 private:
-    Team *team1;   // Private member variable
-    Team *team2;   // Private member variable
-    string result; // Private member variable
-
-    static int matchCount; // Static variable to count the number of matches played
+    Team* team1;
+    Team* team2;
+    string result;
+    static int matchCount;
 
 public:
-    // Default constructor
-    Match()
-    {
-        team1 = nullptr;
-        team2 = nullptr;
+    Match(Team* t1, Team* t2) : SportsTeam(t1->getTeamName() + " vs " + t2->getTeamName()), team1(t1), team2(t2) {
         result = "Match not played yet.";
         matchCount++;
-        cout << "Match Default Constructor called.\n";
     }
 
-    // Parameterized constructor to initialize the match with two teams
-    Match(Team *t1, Team *t2) : team1(t1), team2(t2)
-    {
-        result = "Match not played yet.";
-        matchCount++;
-        cout << "Match Parameterized Constructor called.\n";
+    void playMatch() {
+        if (team1->getPlayerCount() > team2->getPlayerCount()) {
+            result = team1->getTeamName() + " wins!";
+        } else if (team1->getPlayerCount() < team2->getPlayerCount()) {
+            result = team2->getTeamName() + " wins!";
+        } else {
+            result = "It's a draw!";
+        }
+        cout << "Result: " << result << endl;
     }
 
-    // Destructor
-    ~Match()
-    {
-        cout << "Match Destructor called.\n";
-        matchCount--; // Decrease match count when a match is destroyed
+    void displayResult() const {
+        cout << "Match Result: " << result << endl;
     }
 
-    // Method to simulate the match and determine the winner
-    void playMatch()
-    {
-        // Logic to decide the result of the match (e.g., based on number of players)
-        result = "Match played. Result: Draw.";
-        cout << result << endl;
-    }
-
-    // Static method to get the total number of matches played
-    static int getMatchCount()
-    {
+    static int getMatchCount() {
         return matchCount;
+    }
+
+    static void resetMatchCount() {
+        matchCount = 0;
+    }
+
+    ~Match() {
+        delete team1;
+        delete team2;
     }
 };
 
-int Match::matchCount = 0; // Initialize static match count
+// Initialize static variable for Match
+int Match::matchCount = 0;
 
 // Function to get player names from the user
-vector<Player> getPlayers(int numPlayers)
-{
+vector<Player> getPlayers(int numPlayers) {
     vector<Player> players;
-    for (int i = 0; i < numPlayers; ++i)
-    {
+    for (int i = 0; i < numPlayers; ++i) {
         string playerName;
         cout << "Enter name of player " << (i + 1) << ": ";
         cin >> playerName;
-        players.push_back(Player(playerName)); // Using the parameterized constructor
+        players.push_back(Player(playerName));
     }
     return players;
 }
 
-int main()
-{
-    // Get team A details from the user
+// Main function
+int main() {
     string teamAName;
     int numPlayersA;
     cout << "Enter the name of Team A: ";
@@ -186,9 +145,8 @@ int main()
     cout << "Enter the number of players in Team A: ";
     cin >> numPlayersA;
     vector<Player> playersA = getPlayers(numPlayersA);
-    Team *teamA = new Team(teamAName, playersA); // Using the parameterized constructor
+    Team* teamA = new Team(teamAName, playersA);
 
-    // Get team B details from the user
     string teamBName;
     int numPlayersB;
     cout << "Enter the name of Team B: ";
@@ -196,17 +154,36 @@ int main()
     cout << "Enter the number of players in Team B: ";
     cin >> numPlayersB;
     vector<Player> playersB = getPlayers(numPlayersB);
-    Team *teamB = new Team(teamBName, playersB); // Using the parameterized constructor
+    Team* teamB = new Team(teamBName, playersB);
 
-    // Create a match between Team A and Team B
-    Match *match1 = new Match(teamA, teamB); // Using the parameterized constructor
+    cout << "\nInitial Teams:\n";
+    teamA->displayTeam();
+    teamB->displayTeam();
 
-    // Play the match
+    string newPlayerName;
+    cout << "\nEnter name of a new player to add to Team A: ";
+    cin >> newPlayerName;
+    Player newPlayer(newPlayerName);
+    teamA->addPlayer(newPlayer);
+
+    cout << "\nUpdated Teams:\n";
+    teamA->displayTeam();
+    teamB->displayTeam();
+
+    cout << "\nPlaying a match between " << teamA->getTeamName() << " and " << teamB->getTeamName() << "...\n";
+    Match* match1 = new Match(teamA, teamB);
     match1->playMatch();
+    match1->displayResult();
 
-    // Clean up (delete the dynamically allocated objects)
-    delete teamA;
-    delete teamB;
+    cout << "\nTotal number of teams created: " << Team::getTeamCount() << endl;
+    cout << "Total number of matches played: " << Match::getMatchCount() << endl;
+
+    Team::resetTeamCount();
+    Match::resetMatchCount();
+    cout << "\nTeam and Match counts reset." << endl;
+    cout << "Total number of teams created: " << Team::getTeamCount() << endl;
+    cout << "Total number of matches played: " << Match::getMatchCount() << endl;
+
     delete match1;
 
     return 0;
